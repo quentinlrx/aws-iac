230fd1f3d6fdac5907169bc348e7a7f1ba15383f
quentinlrx <46523594+quentinlrx@users.noreply.github.com>
2022-11-04 14:43:20 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,78 +2,126 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 4.16"
     }
   }
+
+  required_version = ">= 1.2.0"
 }
+
 provider "aws" {
-  region = var.region
-  shared_credentials_file = var.creds
-  profile = "default"
+  region  = "us-east-1"
 }
-resource "aws_vpc" "app_vpc" {
-  cidr_block = var.vpc_cidr
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-09d3b3274b6c5d4aa"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.publicsubnets.id
+  associate_public_ip_address = true
+  vpc_security_group_ids = [aws_security_group.SecurityGroup.id] 
+  user_data_replace_on_change = true
+  user_data = <<EOF
+#!/bin/bash
+yum update -y
+amazon-linux-extras install -y php7.2
+yum install -y httpd 
+systemctl start httpd
+systemctl enable httpd
+usermod -a -G apache ec2-user
+chown -R ec2-user:apache /var/www
+chmod 2775 /var/www
+find /var/www -type d -exec chmod 2775 {} \;
+find /var/www -type f -exec chmod 0664 {} \;
+echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php
+yum install -y git
+cd /var/www/
+git clone https://bitbucket.org/fhoubart/testphaser_aws.git 
+mv testphaser_aws/public_html/* html
+EOF 
+  
 
   tags = {
-    Name = "app-vpc"
+    Name = "Web"
   }
+}
+
+resource "aws_vpc" "myvpc" {
+  cidr_block = "10.0.0.0/24"
+  instance_tenancy = "default"
 }
 
 resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.app_vpc.id
-
-  tags = {
-    Name = "vpc_igw"
-  }
+    vpc_id = aws_vpc.myvpc.id
 }
 
-resource "aws_subnet" "public_subnet" {
-  vpc_id            = aws_vpc.app_vpc.id
-  cidr_block        = var.public_subnet_cidr
-  map_public_ip_on_launch = true
-  availability_zone = "us-west-2a"
-
-  tags = {
-    Name = "public-subnet"
-  }
+resource "aws_subnet" "publicsubnets" {
+    vpc_id = aws_vpc.myvpc.id
+    cidr_block = "10.0.0.0/26"
 }
 
-resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.app_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-  tags = {
-    Name = "public_rt"
-  }
+resource "aws_subnet" "privatesubnets" {
+    vpc_id = aws_vpc.myvpc.id
+    cidr_block = "10.0.0.64/26"
 }
 
-resource "aws_route_table_association" "public_rt_asso" {
-  subnet_id      = aws_subnet.public_subnet.id
-  route_table_id = aws_route_table.public_rt.id
-}
-resource "aws_instance" "web" {
-    ami             = "ami-005e54dee72cc1d00" 
-    instance_type   = var.instance_type
-    key_name        = var.instance_key
-    subnet_id       = aws_subnet.public_subnet.id
-  
-    user_data = <<-EOF
-    #!/bin/bash 
-    echo "*** Installing apache2"
-    sudo apt update -y
-    sudo apt install apache2 -y
-    echo "*** Completed Installing apache2"
-    EOF
-  
-    tags = {
-      Name = "web_instance"
+resource "aws_route_table" "PubicRt" {
+    vpc_id = aws_vpc.myvpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.igw.id
     }
-  
-    volume_tags = {
-      Name = "web_instance"
-    } 
+}
+
+resource "aws_route_table" "PrivateRT" {
+    vpc_id = aws_vpc.myvpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = aws_nat_gateway.NATgw.id
+    }
+}
+
+resource "aws_route_table_association" "PublicRtassociation" {
+    subnet_id = aws_subnet.publicsubnets.id
+    route_table_id = aws_route_table.PubicRt.id
+}
+
+resource "aws_route_table_association" "PrivateRtassociation" {
+    subnet_id = aws_subnet.privatesubnets.id
+    route_table_id = aws_route_table.PrivateRT.id
+}
+
+resource "aws_eip" "nateIP" {
+    vpc = true
+}
+
+resource "aws_nat_gateway" "NATgw" {
+    allocation_id = aws_eip.nateIP.id
+    subnet_id = aws_subnet.publicsubnets.id
+}
+
+resource "aws_security_group" "SecurityGroup" {
+    name = "secgroupname"
+    description = "secgroupname"
+    vpc_id      = aws_vpc.myvpc.id
+    ingress {
+        from_port = 22
+        protocol = "tcp"
+        to_port = 22
+        cidr_blocks = ["0.0.0.0/0"]
+      }
+
+      
+    ingress {
+        from_port = 80
+        protocol = "tcp"
+        to_port = 80
+        cidr_blocks = ["0.0.0.0/0"]
+      }
+
+    egress {
+        from_port = 0
+        protocol = "-1"
+        to_port = 0
+        cidr_blocks = ["0.0.0.0/0"]
+      }
   }
